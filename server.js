16c6cbbe99a1e6d8ad29e03cb1eab8ccec91e583
Nagesh Tiwari <ngstwr@gmail.com>
2022-01-25 11:37:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -103,7 +103,9 @@ httpApp = httpApp.listen(process.env.PORT || PORT, process.env.IP || "0.0.0.0", 
 
 ioServer(httpApp, {
     cors: {
-        origin: "*"
+        origin: "*",
+        withCredentials: false,
+        methods: ["GET", "POST"]
     }
 }).on('connection', function(socket) {
     RTCMultiConnectionServer.addSocket(socket, config);
